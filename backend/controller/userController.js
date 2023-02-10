dea90ba1c153f6f1988f463ad5e6e753e0e982b1
Tusharsolanki077 <tusharsolanki077@gmail.com>
2023-02-10 22:13:57 +0530

    added loginUser in controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -13,3 +13,30 @@ exports.registerUser = async(req,res,next) => {
         user,
     });
 };
+
+// login user
+exports.loginUser = async (req,res,next) => {
+
+    const {email , password} = req.body;
+
+    if(!email || !password){
+        return next(new ErrorHander("Please Enter username and password",400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user){
+        return next(new ErrorHander("Invalid email or password",401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHander("Invalid email or password",401));
+    }
+    
+    res.status(200).json({
+        success: true,
+        user,
+    });
+}
